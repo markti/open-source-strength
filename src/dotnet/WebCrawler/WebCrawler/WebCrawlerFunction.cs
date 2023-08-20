88624f944e92fdaa581e32fa7663dff01087c1d6
markti <markti@outlook.com>
2023-08-20 13:02:19 -0400

    web crawler function not static

---
+++ src/dotnet/WebCrawler/WebCrawler/WebCrawlerFunction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace WebCrawler
+{
+    public class WebCrawlerFunction
+    {
+        private readonly ILogger<WebCrawlerFunction> _logger;
+
+        public WebCrawlerFunction(ILogger<WebCrawlerFunction> logger)
+        {
+            _logger = logger;
+        }
+
+        [FunctionName("page-crawler")]
+        public async Task<IActionResult> CrawlPage(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "page")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            return new OkResult();
+        }
+    }
+}
