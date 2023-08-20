c4ad8508036f96d091a612aac8e78324807328d4
markti <markti@outlook.com>
2023-08-20 14:11:39 -0400

    add daily crawler

---
+++ src/dotnet/WebCrawler/WebCrawler/DailyCrawler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using WebCrawler.Services.Interfaces;
+
+namespace WebCrawler
+{
+    public class DailyCrawler
+    {
+        private readonly ILogger<DailyCrawler> _logger;
+        private readonly IPageProcessor _pageProcessor;
+
+        public DailyCrawler(ILogger<DailyCrawler> logger, IPageProcessor pageProcessor)
+        {
+            _logger = logger;
+            _pageProcessor = pageProcessor;
+        }
+
+        [FunctionName("daily-crawler")]
+        public async Task Run([TimerTrigger("0 0 0 * * *")]TimerInfo myTimer)
+        {
+            var results = await _pageProcessor.ProcessPageAsync();
+        }
+    }
+}
