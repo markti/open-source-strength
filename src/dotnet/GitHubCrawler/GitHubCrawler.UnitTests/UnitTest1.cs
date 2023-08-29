7cd0d24ce13a1b4042b80aa1e6e357b1d7105477
markti <markti@outlook.com>
2023-08-28 20:09:38 -0400

    add unit tests ^_^

--- src/dotnet/GitHubCrawler/GitHubCrawler.UnitTests/UnitTest1.cs
+++ src/dotnet/GitHubCrawler/GitHubCrawler.UnitTests/UnitTest1.cs
@@ -4,8 +4,10 @@ namespace GitHubCrawler.UnitTests;
 
 public class UnitTest1
 {
+    [Trait("Category", "Unit")]
     [Fact]
     public void Test1()
     {
+        Assert.True(true); // YES! QUALITY!!!
     }
 }
